58b0f00a4e1bd395fae6e1b0d1fbe348baaa0654
B A C <117440896+PUSHKAR2521@users.noreply.github.com>
2024-04-22 18:58:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,8 +37,8 @@ module.exports = {
 
   DASHBOARD: {
     enabled: true, // enable or disable dashboard
-    baseURL: "http://localhost:8080/api/callback", // base url
-    failureURL: "https://localhost:8080", // failure redirect url
+    baseURL: "https://my-bot-o92m.onrender.com/api/callback", // base url
+    failureURL: "https://my-bot-o92m.onrender.com", // failure redirect url
     port: "8080", // port to run the bot on
   },
 
