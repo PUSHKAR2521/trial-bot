e40451f65eda840c9afbf595e49d6497749008d9
B A C <117440896+PUSHKAR2521@users.noreply.github.com>
2024-04-30 18:21:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -59,10 +59,10 @@ module.exports = {
     // Refer to https://github.com/freyacodes/Lavalink to host your own lavalink server
     LAVALINK_NODES: [
       {
-        host: "lavalink.api.timelessnesses.me",
-        port: 80,
-        password: "youshallnotpass",
-        id: "Timelessnesses Connected",
+        host: "lavalink.devamop.in",
+        port: 443,
+        password: "DevamOP",
+        id: "DevamOp",
         secure: false,
       },
     ],
