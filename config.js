3d797aafa19a8a67ddc161c696162f7292b84b55
B A C <117440896+PUSHKAR2521@users.noreply.github.com>
2024-07-13 11:58:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -52,7 +52,7 @@ module.exports = {
   },
 
   MUSIC: {
-    ENABLED: true,
+    ENABLED: false,
     IDLE_TIME: 1440, // Time in seconds before the bot disconnects from an idle voice channel
     MAX_SEARCH_RESULTS: 5,
     DEFAULT_SOURCE: "YT", // YT = Youtube, YTM = Youtube Music, SC = SoundCloud
